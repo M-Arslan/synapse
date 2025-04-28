4f5df9b301c2c723c5c0a9350b15e20d95abed9b
Arslan Tariq <arslantariq25@gmail.com>
2025-04-29 02:03:22 +0500

    Vercel Configuration
    Vercel Configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { createServer, proxy } from 'aws-serverless-express';
+
+// Create an Express app
+const expressApp = express();
+const adapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, adapter);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3001);
+  await app.init();
 }
 bootstrap();
+
+
+const server = createServer(expressApp);
+export const handler = (event, context) => proxy(server, event, context);
